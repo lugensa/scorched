3c5c7282154f8ff64aca8d9db2cb7ce64434537c
Mauro Amico <mauro.amico@gmail.com>
2015-07-31 15:36:13 +0200

    search timeout

--- scorched/tests/test_connection.py
+++ scorched/tests/test_connection.py
@@ -1,9 +1,19 @@
 from __future__ import unicode_literals
+import json
 import os
 import mock
 import requests
 import unittest
 import scorched.connection
+
+HTTPBIN = os.environ.get('HTTPBIN_URL', 'http://httpbin.org/')
+# Issue #1483: Make sure the URL always has a trailing slash
+HTTPBIN = HTTPBIN.rstrip('/') + '/'
+
+
+def httpbin(*suffix):
+    """Returns url for HTTPBIN resource."""
+    return requests.compat.urljoin(HTTPBIN, '/'.join(suffix))
 
 
 class TestConnection(unittest.TestCase):
@@ -120,3 +130,29 @@ class TestConnection(unittest.TestCase):
         self.assertRaises(ValueError, sc.url_for_update, optimize=True, maxSegments="a")
         self.assertRaises(ValueError, sc.url_for_update, optimize=True, maxSegments=-1)
         self.assertRaises(ValueError, sc.url_for_update, maxSegments=2)
+
+    def test_select_timeout(self):
+        dsn = "http://localhost:1234/none"
+        # max_length_get_url=99999: httbin doesn't support POST
+        sc = scorched.connection.SolrConnection(
+            url=dsn, http_connection=None, mode="", retry_timeout=-1,
+            max_length_get_url=99999, search_timeout=3.0)
+        sc.select_url = httpbin('delay/2')
+        # delay 2.0s < 3.0s timeout, ok
+        resp = sc.select([])
+        self.assertTrue(json.loads(resp)['url'].startswith(sc.select_url))
+        # delay 2.0s > 1.0s timeout, raise ReadTimeout
+        sc.search_timeout = 1.0
+        self.assertRaises(requests.exceptions.ReadTimeout, sc.select, [])
+        sc.search_timeout = (5.0, 1.0)  # (connect, read)
+        self.assertRaises(requests.exceptions.ReadTimeout, sc.select, [])
+        # delay 2.0s < 3.0s timeout, ok
+        sc.search_timeout = (1.0, 3.0)  # (connect, read)
+        resp = sc.select([])
+        self.assertTrue(json.loads(resp)['url'].startswith(sc.select_url))
+        # Connecting to an invalid port should raise a ConnectionError
+        sc.select_url = "http://httpbin.org:1/none/select"
+        sc.search_timeout = 1.0
+        self.assertRaises(requests.exceptions.ConnectTimeout, sc.select, [])
+        sc.search_timeout = (1.0, 5.0)
+        self.assertRaises(requests.exceptions.ConnectTimeout, sc.select, [])
