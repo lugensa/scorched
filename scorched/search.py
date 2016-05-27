dda358e7993a24055a60a0099dc83e989f941ad5
rlskoeser <rebecca.s.koeser@emory.edu>
2016-05-27 16:38:33 -0400

    Add tests for stats option

--- scorched/search.py
+++ scorched/search.py
@@ -1329,6 +1329,8 @@ class StatOptions(Options):
     opts = {
         "stats.facet": str,
     }
+    # NOTE: solr documentation indicates stats.facet is a legacy parameter,
+    # recommends using stats.field with facet.pivot instead
 
     def __init__(self, original=None):
         if original is None:
