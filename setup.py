68bf685cd20d0ef2fa46dc560bf52a8ab9ed6315
ale-rt <alessandro.pisa@gmail.com>
2021-06-28 16:35:04 +0200

    Always use unittest.mock

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name='scorched',
           "pytz",
       ],
       extras_require={
-          'test': ["nose", "coverage", "mock"],
+          'test': ["nose", "coverage"],
       },
       test_suite='scorched.tests',
       setup_requires=["setuptools_git"],
