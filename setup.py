01699097119b337e03ddc8af001e89b7e52513bf
mghh <mg@lugensa.com>
2022-03-08 14:06:28 +0100

    Add py3.10 to setup classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="solr tow sunburnt offspring",
     author="(Josip Delic) Lugensa GmbH",
